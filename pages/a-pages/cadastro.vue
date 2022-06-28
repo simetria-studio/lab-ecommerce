1a04f58803de806d3f3916a1989c0b2a13eb69a2
Felipe Cruz <felipephplow@gmail.com>
2022-06-28 16:46:51 -0300

    dwa

--- pages/a-pages/cadastro.vue
+++ pages/a-pages/cadastro.vue
@@ -155,14 +155,100 @@
           </div>
           <div v-if="step === 3">
             <div class="row">
-              
-              <div class="col-md-3 bt-3">
+              <div class="col-md-3 my-2">
+                <button class="btn btn-two" @click.prevent="direito()">
+                  Lado Direito
+                </button>
+              </div>
+              <div class="col-md-3 my-2">
+                <button class="btn btn-two">Lado Esquerdo</button>
+              </div>
+              <div class="col-md-3 my-2">
+                <button class="btn btn-two">Focinho</button>
+              </div>
+              <div class="col-md-4 my-2">
+                <button class="btn btn-two">Linha Superior dos olhos</button>
+              </div>
+              <div class="col-md-5 my-2">
+                <button class="btn btn-two">Pescoço vista inferior</button>
+              </div>
+              <div class="col-md-5 my-2">
+                <button class="btn btn-two">
+                  Membros posterior e vista posterior
+                </button>
+              </div>
+              <div class="col-md-4 my-2">
+                <button class="btn btn-two">
+                  Membros anterioes vista posterior
+                </button>
+              </div>
+              <div class="col-md-5 bt-3">
                 <button class="btn btn-two" @click.prevent="prev()">
                   Anterior
                 </button>
                 <button class="btn btn-one" @click.prevent="next()">
-                  Próximo
+                  Finalizar
                 </button>
+              </div>
+            </div>
+          </div>
+          <div v-if="lado === 'd'">
+            <div class="row">
+              <div class="col-md-4">
+                <div>
+                  <h3>Lado direito</h3>
+                </div>
+                <div>
+                  <p>fotos</p>
+                </div>
+                <div class="fotos">
+                  <div>
+                    <div class="form-group">
+                      <input
+                        type="file"
+                        class="form-control-file"
+                        id="exampleFormControlFile1"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div class="form-group">
+                    <label for="exampleFormControlInput1">Marcas</label>
+                    <input
+                      type="search"
+                      class="form-control"
+                      id="exampleFormControlInput1"
+                      placeholder="Buscar..."
+                    />
+                  </div>
+                </div>
+                <div></div>
+              </div>
+              <div class="col-md-7">
+                <h3>Foto do animal (Imagine)</h3>
+                <div
+                  style="
+                    height: 500px;
+                    width: 500px;
+                    border: 1px solid red;
+                    position: absolute;
+                  "
+                >
+                  <vue-draggable-resizable
+                    :w="100"
+                    :h="100"
+                    class="img-teste"
+                    @dragging="onDrag"
+
+                    :parent="false"
+                    :list="list1"
+                    v-for="element in list1"
+                    :key="element.name"
+                  >
+                    <p >{{ element.name }}</p>
+                  </vue-draggable-resizable>
+                </div>
               </div>
             </div>
           </div>
@@ -173,14 +259,30 @@
 </template>
 <script>
 import MenuComponent from "~/components/dashboard/MenuComponent.vue";
+import VueDraggableResizable from "vue-draggable-resizable";
+
 export default {
-  components: { MenuComponent },
+  components: { MenuComponent, VueDraggableResizable },
   middleware: "authenticated",
+
   data() {
     return {
       step: 1,
+      lado: "",
+      width: 0,
+      height: 0,
+      x: 0,
+      y: 0,
+      list1: [
+        { name: "Marca1", id: 1 },
+        { name: "Marca2", id: 2 },
+        { name: "Marca3", id: 3 },
+        { name: "Marca4", id: 4 },
+        { name: "Marca5", id: 5 },
+      ],
     };
   },
+
   methods: {
     prev() {
       this.step--;
@@ -188,8 +290,23 @@ export default {
     next() {
       this.step++;
     },
+
+    direito() {
+      this.lado = "d";
+      this.step++;
+    },
     submit() {
       alert("Submit to blah and show blah and etc.");
+    },
+    onResize: function (x, y, width, height) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+    },
+    onDrag: function (x, y) {
+      this.x = x;
+      this.y = y;
     },
   },
 };
